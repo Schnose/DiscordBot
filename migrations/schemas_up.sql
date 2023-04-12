f4ce8b9f51b63c403439172eec8a4b7fdb159df5
AlphaKeks <alphakeks@dawn.sh>
2023-04-12 09:07:34 +0200

    (fix): `mode` should be an int

--- migrations/schemas_up.sql
+++ migrations/schemas_up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
 	name VARCHAR(255) NOT NULL,
 	discord_id BIGINT NOT NULL UNIQUE,
 	steam_id INT,
-	mode VARCHAR(255),
+	mode INT4,
 
 	PRIMARY KEY (discord_id)
 );
